f9bd919d8fdf61eaaac2fc27cd0b77a0d0461f21
reri <reri0702@gmail.com>
2025-12-04 11:14:56 +0900

    [type] add  > file compression to zip

--- dirbackup/src/main/java/com/reri72/dirbackup/compress/ZipCompress.java
+++ dirbackup/src/main/java/com/reri72/dirbackup/compress/ZipCompress.java
@@ -1,7 +1,9 @@
 package com.reri72.dirbackup.compress;
 
-import java.io.*;
-import java.nio.file.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -15,5 +17,40 @@ public class ZipCompress implements Compress {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmssSSS");
         String now = LocalDateTime.now().format(formatter);
         System.out.println(now);
+
+        String zipFilePath = destPath+"/backup_" + now + ".zip";
+
+        try (FileOutputStream fos = new FileOutputStream(zipFilePath);
+                ZipOutputStream zos = new ZipOutputStream(fos))
+        {
+            byte[] buffer = new byte[1024];
+
+            for (String fileName : fileNames)
+            {
+                try (FileInputStream fis = new FileInputStream(fileName))
+                {
+                    ZipEntry zipEntry = new ZipEntry(fileName);
+                    zos.putNextEntry(zipEntry);
+
+                    int byteRead;
+                    while ((byteRead = fis.read(buffer)) != -1)
+                    {
+                        zos.write(buffer, 0, byteRead);
+                    }
+
+                    zos.closeEntry();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            throw e;
+        }
     }
+
 }
