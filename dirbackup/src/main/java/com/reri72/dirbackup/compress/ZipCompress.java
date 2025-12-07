84d722dc0f1d4f8962db0a51c87ccec5a94c9119
reri <reri0702@gmail.com>
2025-12-07 10:46:10 +0900

    [type] add  > zip compression for general files

--- dirbackup/src/main/java/com/reri72/dirbackup/compress/ZipCompress.java
+++ dirbackup/src/main/java/com/reri72/dirbackup/compress/ZipCompress.java
@@ -1,15 +1,35 @@
 package com.reri72.dirbackup.compress;
 
+import java.io.IOException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.zip.*;
 
 public class ZipCompress implements Compress {
+
+    private void addFileToZip(File file, String entryName, ZipOutputStream zos) throws IOException
+    {
+        System.out.println("압축 중 (파일): " + entryName);
+        try (FileInputStream fis = new FileInputStream(file))
+        {
+            ZipEntry zipEntry = new ZipEntry(entryName);
+            zos.putNextEntry(zipEntry);
+
+            byte[] buffer = new byte[1024];
+            int byteRead;
+            while ((byteRead = fis.read(buffer)) != -1)
+            {
+                zos.write(buffer, 0, byteRead);
+            }
+
+            zos.closeEntry();
+        }
+    }
 
     @Override
     public void compress(String sourcePath, List<String> fileNames, String destPath) throws IOException
@@ -23,26 +43,17 @@ public class ZipCompress implements Compress {
         try (FileOutputStream fos = new FileOutputStream(zipFilePath);
                 ZipOutputStream zos = new ZipOutputStream(fos))
         {
-            byte[] buffer = new byte[1024];
-
             for (String fileName : fileNames)
             {
-                try (FileInputStream fis = new FileInputStream(fileName))
+                File file = new File(fileName);
+
+                if (file.isDirectory())
                 {
-                    ZipEntry zipEntry = new ZipEntry(fileName);
-                    zos.putNextEntry(zipEntry);
 
-                    int byteRead;
-                    while ((byteRead = fis.read(buffer)) != -1)
-                    {
-                        zos.write(buffer, 0, byteRead);
-                    }
-
-                    zos.closeEntry();
                 }
-                catch (IOException e)
+                else // 일반 파일
                 {
-                    e.printStackTrace();
+                    addFileToZip(file, fileName, zos);
                 }
             }
         }
