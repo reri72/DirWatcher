ef569845ebce58bed6b5a762519950a7aef4b420
reri <reri0702@gmail.com>
2025-12-09 09:53:18 +0900

    [type] add  > define default method in Compress interface

--- dirbackup/src/main/java/com/reri72/dirbackup/compress/Compress.java
+++ dirbackup/src/main/java/com/reri72/dirbackup/compress/Compress.java
@@ -2,8 +2,17 @@ package com.reri72.dirbackup.compress;
 
 import java.io.IOException;
 import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 // 인터페이스
 public interface Compress {
-    void compress(List<String> fileNames, String destPath) throws IOException;
+    public void compress(List<String> fileNames, String destPath) throws IOException;
+
+    // 디폴트 메서드 (공통 사용)
+    default String getTimestamp()
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmssSSS");
+        return LocalDateTime.now().format(formatter);
+    }
 }
